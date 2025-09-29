43fcd16848fb165958dd90e380298423bd9baff5
gaurahari-dasa <ganesh.hegde@hkm-group.org>
2025-09-29 11:29:08 +0530

    Home component uses Layout, Haribol

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,3 +1,26 @@
+<script setup>
+import Layout from "./Layout.vue";
+</script>
+
 <template>
-    <p class="font-poppins text-2xl">Haribol</p>
+    <Layout>
+        <div class="flex-col">
+            <div class="relative">
+                <img
+                    src="../../images/ban-logo.png"
+                    alt="Banner Logo"
+                    class="absolute top-6 right-10 w-[43%]"
+                />
+                <img src="../../images/Top-banner.jpg" alt="Top Banner" />
+                <h1
+                    class="absolute top-1/2 right-10 text-center font-poppins text-3xl text-gray-200"
+                >
+                    The Official Website of<br /><span class="text-4xl font-medium"
+                        >Sri Chanchalapathi Dasa</span
+                    >
+                </h1>
+            </div>
+            <p class="font-poppins text-2xl">Haribol</p>
+        </div>
+    </Layout>
 </template>
