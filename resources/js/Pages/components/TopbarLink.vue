e7c79b423dfb0ec58abfd917400ae2d08ed1379a
gaurahari-dasa <ganesh.hegde@hkm-group.org>
2025-09-29 11:27:59 +0530

    Layout component, Haribol

---
+++ resources/js/Pages/components/TopbarLink.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+defineProps(["href"]);
+</script>
+
+<template>
+    <Link :href class="font-semibold uppercase"><slot /></Link>
+</template>
