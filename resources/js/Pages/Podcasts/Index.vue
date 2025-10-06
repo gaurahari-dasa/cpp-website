c35a9954f0661443ac0c276583e543964b48a7e4
gaurahari-dasa <ganesh.hegde@hkm-group.org>
2025-10-06 18:07:31 +0530

    Podcast popup done, Haribol

--- resources/js/Pages/Podcasts/Index.vue
+++ resources/js/Pages/Podcasts/Index.vue
@@ -5,13 +5,27 @@ import DoubleArrow from "../components/DoubleArrow.vue";
 import ContentBuffer from "../components/ContentBuffer";
 import LeftArrowHead from "../components/LeftArrowHead.vue";
 import RightArrowHead from "../components/RightArrowHead.vue";
+import Details from "./components/Details.vue";
+
+import { ref } from "vue";
 
 const props = defineProps(["baseUrl"]);
 const buffer = new ContentBuffer(`${props.baseUrl}/podcasts/carousel`, 3, 1);
+const detailCard = ref(null);
+
+function displayDetails(card) {
+    detailCard.value = card;
+    window.scrollTo(0, 0);
+}
 </script>
 <template>
     <Layout active="Home">
         <div class="font-poppins">
+            <Details
+                v-if="detailCard"
+                :card="detailCard"
+                @close="detailCard = null"
+            />
             <div
                 class="flex flex-col items-center bg-[url(../images/home-page/bg1.jpg)] py-24"
             >
@@ -19,41 +33,48 @@ const buffer = new ContentBuffer(`${props.baseUrl}/podcasts/carousel`, 3, 1);
                     >Podcasts</SectionHeading
                 >
                 <DoubleArrow />
-                <div class="mt-16 grid grid-cols-[3fr_4fr_3fr] items-center">
+                <div :class="detailCard ? 'invisible' : 'visible'">
                     <div
-                        class="relative p-4"
-                        v-for="(card, ix) in buffer.window.value"
+                        class="mt-16 grid grid-cols-[3fr_4fr_3fr] items-center"
                     >
-                        <img
-                            class="w-full shadow-xl"
-                            :src="card.page_image"
-                            alt="Podcast Image"
-                        />
-                        <LeftArrowHead
-                            @click="buffer.backward(1)"
-                            v-show="ix === 0"
-                            class="absolute top-1/2 left-2 size-10 -translate-y-1/2"
-                        />
-                        <RightArrowHead
-                            @click="buffer.forward(1)"
-                            v-show="ix === buffer.window.value.length - 1"
-                            class="absolute top-1/2 right-2 size-10 -translate-y-1/2"
-                        />
+                        <div
+                            class="relative p-4"
+                            v-for="(card, ix) in buffer.window.value"
+                        >
+                            <img
+                                class="w-full shadow-xl"
+                                :src="card.page_image"
+                                alt="Podcast Image"
+                                @click="displayDetails(card)"
+                            />
+                            <LeftArrowHead
+                                @click="buffer.backward(1)"
+                                v-show="ix === 0"
+                                class="absolute top-1/2 left-2 size-10 -translate-y-1/2"
+                            />
+                            <RightArrowHead
+                                @click="buffer.forward(1)"
+                                v-show="ix === buffer.window.value.length - 1"
+                                class="absolute top-1/2 right-2 size-10 -translate-y-1/2"
+                            />
+                        </div>
                     </div>
-                </div>
-                <div
-                    class="mt-4 grid grid-cols-[3fr_4fr_3fr] items-center gap-4"
-                >
-                    <div v-for="(card, ix) in buffer.window.value">
-                        <div v-show="ix === 1">
-                            <p class="text-center text-3xl text-deepblue-500">
-                                {{ card.page_title }}
-                            </p>
-                            <p
-                                class="mt-4 text-center text-base/relaxed text-gray-500"
-                            >
-                                {{ card.page_text }}
-                            </p>
+                    <div
+                        class="mt-4 grid grid-cols-[3fr_4fr_3fr] items-center gap-4"
+                    >
+                        <div v-for="(card, ix) in buffer.window.value">
+                            <div v-show="ix === 1">
+                                <p
+                                    class="text-center text-3xl text-deepblue-500"
+                                >
+                                    {{ card.page_title }}
+                                </p>
+                                <p
+                                    class="mt-4 text-center text-base/relaxed text-gray-500"
+                                >
+                                    {{ card.page_text }}
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
---
+++ resources/js/Pages/Podcasts/components/Details.vue
@@ -0,0 +1,28 @@
+<script setup>
+import SectionHeading from "../../components/SectionHeading.vue";
+import { XMarkIcon } from "@heroicons/vue/16/solid";
+
+defineProps(["card"]);
+defineEmits(["close"]);
+</script>
+<template>
+    <div
+        class="fixed inset-0 z-10 flex items-center justify-center bg-black/50"
+    >
+        <div
+            class="relative flex max-h-5/6 max-w-5/6 flex-col items-center overflow-y-scroll bg-gray-200 px-8 py-10"
+        >
+            <XMarkIcon
+                class="absolute top-1 right-1 size-6"
+                @click="$emit('close')"
+            />
+            <img :src="card.page_image" alt="Podcast Popup Image" />
+            <SectionHeading class="mt-20 text-deepblue-500">{{
+                card.page_title
+            }}</SectionHeading>
+            <p class="mt-8 text-center text-2xl/loose text-deepblue-500">
+                {{ card.page_text }}
+            </p>
+        </div>
+    </div>
+</template>
