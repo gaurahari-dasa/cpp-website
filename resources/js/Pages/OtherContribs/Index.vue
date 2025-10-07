3c5b1d5f9568182c5327b144365aefd9d2d69f9a
gaurahari-dasa <ganesh.hegde@hkm-group.org>
2025-10-07 10:57:28 +0530

    Other Contributions done, Haribol

--- resources/js/Pages/OtherContribs/Index.vue
+++ resources/js/Pages/OtherContribs/Index.vue
@@ -3,7 +3,7 @@ import Layout from "../components/Layout.vue";
 </script>
 <template>
     <Layout active="Home">
-        <div class="px-16 py-20 font-poppins">
+        <div class="flex flex-col gap-20 px-16 py-20 font-poppins">
             <div class="flex items-start gap-16">
                 <img
                     class="w-[30%] shrink-0"
@@ -11,9 +11,9 @@ import Layout from "../components/Layout.vue";
                     alt="Other Contributions"
                 />
                 <div class="-mt-2">
-                    <p class="text-3xl font-semibold text-deepblue-500">
+                    <h3 class="text-3xl font-semibold text-deepblue-500">
                         Cultural Centers
-                    </p>
+                    </h3>
                     <p class="mt-4 text-base/relaxed text-gray-500">
                         Lorem ipsum dolor sit amet, consectetur adipisicing
                         elit. Neque tempora dolores atque. Incidunt commodi
@@ -33,8 +33,148 @@ import Layout from "../components/Layout.vue";
                     </p>
                     <a
                         class="mt-2 font-medium text-deepblue-500"
-                        href="http://www.iskconcenters.org"
+                        href="www.iskconcenters.org"
                         >www.iskconcenters.org</a
+                    >
+                </div>
+            </div>
+
+            <div class="flex items-start gap-16">
+                <div class="-mt-2">
+                    <h3 class="text-3xl font-semibold text-deepblue-500">
+                        FOLK
+                    </h3>
+                    <p class="mt-4 text-base/relaxed text-gray-500">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Neque tempora dolores atque. Incidunt commodi
+                        excepturi in. Quia accusantium sequi, perferendis
+                        impedit blanditiis officiis voluptates deleniti quis
+                        optio deserunt eum dignissimos, vel similique animi
+                        delectus. Qui quo provident consectetur recusandae amet
+                        soluta, reprehenderit ullam est, incidunt quidem maiores
+                        harum nobis dolores molestias voluptas modi accusantium
+                        explicabo adipisci dolorum autem corrupti blanditiis
+                        eligendi. Earum, laboriosam atque natus quia quod rem
+                        veritatis sit. Praesentium veritatis ut tenetur
+                        assumenda odio eos vero quae sint nihil dolorem minus
+                        magni aspernatur, asperiores fuga a corporis nulla
+                        eligendi, ea earum non? Mollitia odit nostrum sequi
+                        provident atque?
+                    </p>
+                    <a
+                        class="mt-2 font-medium text-deepblue-500"
+                        href="www.folknet.in"
+                        >www.folknet.in</a
+                    >
+                </div>
+                <img
+                    class="w-[30%] shrink-0"
+                    src="../../../images/other-contribs/other contribs 2.jpg"
+                    alt="Other Contributions"
+                />
+            </div>
+
+            <div class="flex items-start gap-16">
+                <img
+                    class="w-[30%] shrink-0"
+                    src="../../../images/other-contribs/other contribs 3.jpg"
+                    alt="Other Contributions"
+                />
+                <div class="-mt-2">
+                    <h3 class="text-3xl font-semibold text-deepblue-500">
+                        ISKCON Samskriti
+                    </h3>
+                    <p class="mt-4 text-base/relaxed text-gray-500">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Neque tempora dolores atque. Incidunt commodi
+                        excepturi in. Quia accusantium sequi, perferendis
+                        impedit blanditiis officiis voluptates deleniti quis
+                        optio deserunt eum dignissimos, vel similique animi
+                        delectus. Qui quo provident consectetur recusandae amet
+                        soluta, reprehenderit ullam est, incidunt quidem maiores
+                        harum nobis dolores molestias voluptas modi accusantium
+                        explicabo adipisci dolorum autem corrupti blanditiis
+                        eligendi. Earum, laboriosam atque natus quia quod rem
+                        veritatis sit. Praesentium veritatis ut tenetur
+                        assumenda odio eos vero quae sint nihil dolorem minus
+                        magni aspernatur, asperiores fuga a corporis nulla
+                        eligendi, ea earum non? Mollitia odit nostrum sequi
+                        provident atque?
+                    </p>
+                    <a
+                        class="mt-2 font-medium text-deepblue-500"
+                        href="www.iskconsamskriti.org"
+                        >www.iskconsamskriti.org</a
+                    >
+                </div>
+            </div>
+
+            <div class="flex items-start gap-16">
+                <div class="-mt-2">
+                    <h3 class="text-3xl font-semibold text-deepblue-500">
+                        NIVE
+                    </h3>
+                    <p class="mt-4 text-base/relaxed text-gray-500">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Neque tempora dolores atque. Incidunt commodi
+                        excepturi in. Quia accusantium sequi, perferendis
+                        impedit blanditiis officiis voluptates deleniti quis
+                        optio deserunt eum dignissimos, vel similique animi
+                        delectus. Qui quo provident consectetur recusandae amet
+                        soluta, reprehenderit ullam est, incidunt quidem maiores
+                        harum nobis dolores molestias voluptas modi accusantium
+                        explicabo adipisci dolorum autem corrupti blanditiis
+                        eligendi. Earum, laboriosam atque natus quia quod rem
+                        veritatis sit. Praesentium veritatis ut tenetur
+                        assumenda odio eos vero quae sint nihil dolorem minus
+                        magni aspernatur, asperiores fuga a corporis nulla
+                        eligendi, ea earum non? Mollitia odit nostrum sequi
+                        provident atque?
+                    </p>
+                    <a
+                        class="mt-2 font-medium text-deepblue-500"
+                        href="www.nive.co.in"
+                        >www.nive.co.in</a
+                    >
+                </div>
+                <img
+                    class="w-[30%] shrink-0"
+                    src="../../../images/other-contribs/other contribs 4.jpg"
+                    alt="Other Contributions"
+                />
+            </div>
+
+            <div class="flex items-start gap-16">
+                <img
+                    class="w-[30%] shrink-0"
+                    src="../../../images/other-contribs/other contribs 5.jpg"
+                    alt="Other Contributions"
+                />
+                <div class="-mt-2">
+                    <h3 class="text-3xl font-semibold text-deepblue-500">
+                        Basil Woods International
+                    </h3>
+                    <p class="mt-4 text-base/relaxed text-gray-500">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Neque tempora dolores atque. Incidunt commodi
+                        excepturi in. Quia accusantium sequi, perferendis
+                        impedit blanditiis officiis voluptates deleniti quis
+                        optio deserunt eum dignissimos, vel similique animi
+                        delectus. Qui quo provident consectetur recusandae amet
+                        soluta, reprehenderit ullam est, incidunt quidem maiores
+                        harum nobis dolores molestias voluptas modi accusantium
+                        explicabo adipisci dolorum autem corrupti blanditiis
+                        eligendi. Earum, laboriosam atque natus quia quod rem
+                        veritatis sit. Praesentium veritatis ut tenetur
+                        assumenda odio eos vero quae sint nihil dolorem minus
+                        magni aspernatur, asperiores fuga a corporis nulla
+                        eligendi, ea earum non? Mollitia odit nostrum sequi
+                        provident atque?
+                    </p>
+                    <a
+                        class="mt-2 font-medium text-deepblue-500"
+                        href="www.basilwoodsinternational.in"
+                        >www.basilwoodsinternational.in</a
                     >
                 </div>
             </div>
